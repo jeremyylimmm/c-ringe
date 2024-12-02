68a5bbb8d0dde85bf59589b1c7b690a1c4b00bbb
jeremyylimmm <jeremylim.0304@gmail.com>
2024-12-02 17:22:47 +1100

    Parse if statement

--- cringe/parse.c
+++ cringe/parse.c
@@ -12,6 +12,8 @@ typedef struct {
     struct { int prec; } binary;
     struct { parse_node_kind_t kind; token_t token; int children_count; } complete;
     struct { token_t lbrace; int count; } block_stmt;
+    struct { token_t if_tok; token_t lparen; } if_body;
+    struct { token_t if_tok; } _else;
   } as;
 } state_t;
 
@@ -82,6 +84,12 @@ static state_t state_block_stmt(token_t lbrace, int count) {
   };
 }
 
+static state_t state_stmt() {
+  return (state_t) {
+    .kind = STATE_STMT
+  };
+}
+
 static state_t state_return() {
   return (state_t) {
     .kind = STATE_RETURN
@@ -100,6 +108,27 @@ static state_t state_complete(parse_node_kind_t kind, token_t token, int childre
     .as.complete.kind = kind,
     .as.complete.token = token,
     .as.complete.children_count = children_count,
+  };
+}
+
+static state_t state_if() {
+  return (state_t) {
+    .kind = STATE_IF
+  };
+}
+
+static state_t state_if_body(token_t if_tok, token_t lparen) {
+  return (state_t) {
+    .kind = STATE_IF_BODY,
+    .as.if_body.if_tok = if_tok,
+    .as.if_body.lparen = lparen
+  };
+}
+
+static state_t state_else(token_t if_tok) {
+  return (state_t) {
+    .kind = STATE_ELSE,
+    .as._else.if_tok = if_tok,
   };
 }
 
@@ -240,7 +269,7 @@ static bool handle_BLOCK(parser_t* p, state_t state) {
 static bool handle_BLOCK_STMT(parser_t* p, state_t state) {
   switch (peek(p).kind) {
     case TOKEN_EOF: {
-      error(p, state.as.block_stmt.lbrace, "this brace has no closing brace");
+      error(p, state.as.block_stmt.lbrace, "no closing '}'");
       return false;
     }
 
@@ -252,20 +281,35 @@ static bool handle_BLOCK_STMT(parser_t* p, state_t state) {
   }
 
   push(p, state_block_stmt(state.as.block_stmt.lbrace, state.as.block_stmt.count + 1));
+  push(p, state_stmt());
+
+  return true;
+}
+
+static bool handle_STMT(parser_t* p, state_t state) {
+  (void)state;
 
   switch (peek(p).kind) {
     default: {
       push(p, state_semi());
       push(p, state_expr());
-      return true;
-    }
+    } break;
 
     case TOKEN_KEYWORD_RETURN: {
       push(p, state_semi());
       push(p, state_return());
-      return true;
-    }
+    } break;
+
+    case '{': {
+      push(p, state_block());
+    } break;
+
+    case TOKEN_KEYWORD_IF: {
+      push(p, state_if());
+    } break;
   }
+
+  return true;
 }
 
 static bool handle_RETURN(parser_t* p, state_t state) {
@@ -293,6 +337,48 @@ static bool handle_SEMI(parser_t* p, state_t state) {
 
 static bool handle_COMPLETE(parser_t* p, state_t state) {
   node(p, state.as.complete.kind, state.as.complete.token, state.as.complete.children_count);
+  return true;
+}
+
+static bool handle_IF(parser_t* p, state_t state) {
+  (void)state;
+
+  token_t if_tok = peek(p);
+  REQUIRE(p, TOKEN_KEYWORD_IF, "expected an 'if' statement");
+
+  token_t lparen = peek(p);
+  REQUIRE(p, '(', "if statement needs an expression: 'if (expr)'");
+
+  push(p, state_if_body(if_tok, lparen));
+  push(p, state_expr());
+
+  return true;
+}
+
+static bool handle_IF_BODY(parser_t* p, state_t state) {
+  if (peek(p).kind != ')') {
+    error(p, state.as.if_body.lparen, "no closing ')'");
+    return false;
+  }
+
+  lex(p);
+
+  push(p, state_else(state.as.if_body.if_tok));
+  push(p, state_stmt());
+
+  return true;
+}
+
+static bool handle_ELSE(parser_t* p, state_t state) {
+  if (peek(p).kind == TOKEN_KEYWORD_ELSE) {
+    node(p, PARSE_NODE_ELSE, lex(p), 0);
+    push(p, state_complete(PARSE_NODE_IF, state.as._else.if_tok, 4));
+    push(p, state_stmt());
+  }
+  else {
+    node(p, PARSE_NODE_IF, state.as._else.if_tok, 2);
+  }
+
   return true;
 }
 
